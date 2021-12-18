9068a5055f98068daba0679b05ed6ef8637fe4cd
Gustavo Taveras <61476293+B0ss-1@users.noreply.github.com>
2021-12-18 16:22:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+// returns driving blocks
+function distanceFromHqInBlocks(end){
+    if (end < 42) { 
+        return 42 - end;
+    } else{            
+        return end - 42;
+    }
+    
+
+}
+function distanceFromHqInFeet(block) {
+    const drivingBlocks = distanceFromHqInBlocks(block)
+    return (drivingBlocks  * 264)
+    
+}
+
+function distanceTravelledInFeet(a, b){
+
+    if (a < b) {
+        return (b - a) * 264;
+    }else {
+        return (a - b) * 264;
+    }
+    
+    
+}
+
+const calculatesFarePrice = (start, destination) => {
+    let travel = Math.abs((destination - start) * 264)
+
+    if (travel <= 400) {
+        return 0;
+    } else if (travel > 400 && travel < 2000) {
+        return (travel - 400) * 0.02;
+    } else if (travel > 2000 && travel < 2500){
+        return 25
+    } else if (travel > 2500) {
+        return "cannot travel that far"
+    }
+}
